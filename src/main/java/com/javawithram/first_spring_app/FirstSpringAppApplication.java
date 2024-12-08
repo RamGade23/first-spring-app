3a2ad4ed196997f1be8b23767c83ebd4c715fc98
Ram Gade <javawithram27@gmail.com>
2024-12-08 22:46:32 +0530

    local changes

--- src/main/java/com/javawithram/first_spring_app/FirstSpringAppApplication.java
+++ src/main/java/com/javawithram/first_spring_app/FirstSpringAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstSpringAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringAppApplication.class, args);
 	}
-
 }
